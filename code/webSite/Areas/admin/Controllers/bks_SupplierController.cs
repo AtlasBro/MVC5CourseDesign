76abb1d1f96ce62a48ef96b059f64aa35070031a
AtlasBro <18205160766@163.com>
2021-06-22 20:20:05 +0800

    6-22
    very f**ked!

--- code/webSite/Areas/admin/Controllers/bks_SupplierController.cs
+++ code/webSite/Areas/admin/Controllers/bks_SupplierController.cs
@@ -23,5 +23,94 @@ namespace web.Areas.admin.Controllers
                 return PartialView("_showData", model);
             return View(model);
         }
+
+        public ActionResult Detail(string rowID)
+        {
+            db.bks_Supplier model = db.bll.bks_Supplier.getEntryByRowID(rowID, dc);
+            return View(model);
+        }
+        //展示新增
+        [HttpGet]
+        public ActionResult Insert()
+        {
+            db.bks_Supplier model = new db.bks_Supplier();
+            return View(model);
+        }
+        //保存新增
+        [HttpPost]
+        public JsonResult Insert(db.bks_Supplier model)
+        {
+            JsonResult result = new JsonResult();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string rowID = db.bll.bks_Supplier.insert(model, dc);
+                    result.Data = rui.jsonResult.getAJAXResult("新增成功", true,
+                        rui.jsonResult.getDicByRowID(rowID));
+                }
+                else
+                {
+                    result.Data = rui.jsonResult.getAJAXResult("输入不合法", false);
+                }
+            }
+            catch (Exception ex)
+            {
+                rui.logHelper.log(ex);
+                result.Data = rui.jsonResult.getAJAXResult(ex.Message, false);
+            }
+            return result;
+        }
+
+        //展示更新
+        [HttpGet]
+        public ActionResult Update(string rowID)
+        {
+            db.bks_Supplier model = db.bll.bks_Supplier.getEntryByRowID(rowID, dc);
+            return View(model);
+        }
+
+        //保存更新
+        [HttpPost]
+        public ActionResult Update(db.bks_Supplier model)
+        {
+            JsonResult result = new JsonResult();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.bll.bks_Supplier.update(model, dc);
+                    result.Data = rui.jsonResult.getAJAXResult("更新成功", true);
+                }
+                else
+                {
+                    result.Data = rui.jsonResult.getAJAXResult("输入不合法", false);
+                }
+            }
+            catch (Exception ex)
+            {
+                rui.logHelper.log(ex);
+                result.Data = rui.jsonResult.getAJAXResult(ex.Message, false);
+            }
+            return result;
+        }
+
+        //删除
+        [HttpPost]
+        public JsonResult Delete(string rowID)
+        {
+            JsonResult result = new JsonResult();
+            try
+            {
+                db.bll.bks_Supplier.delete(rowID, dc);
+                result.Data = rui.jsonResult.getAJAXResult("删除成功", true);
+            }
+            catch (Exception ex)
+            {
+                rui.logHelper.log(ex);
+                result.Data = rui.jsonResult.getAJAXResult(ex.Message, false);
+            }
+            return result;
+        }
 	}
 }
